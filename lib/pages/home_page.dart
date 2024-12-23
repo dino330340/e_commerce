14e0003eebb4fef4ce7076d463d0088342a80353
dino330340 <h.alaaeldin330@gmail.com>
2024-12-23 22:22:29 +0400

    ....

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -36,31 +36,72 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.transparent,
         elevation: 0,
         leading: Builder(builder: (context) {
-          return IconButton(
-            icon: const Icon(
-              Icons.menu,
-              color: Colors.black,
+          return  Padding(
+            padding: const EdgeInsets.only(left: 12.0),
+            child: IconButton(
+              icon:  Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
             ),
-            onPressed: () {
-              Scaffold.of(context).openDrawer();
-            },
           );
         }),
       ),
       drawer: Drawer(
         backgroundColor: Colors.grey[900],
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            DrawerHeader(
-              child: Image.asset(
-                'lib/images/pngwing.com.png',
-                color: Colors.white,
-              ),
+            Column(
+              children: [
+                Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(50.0),
+                    child: Image.asset(
+                      'lib/images/pngwing.com.png',
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Divider(
+                    color: Colors.grey[900],
+                  ),
+                ),
+                const Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: ListTile(
+                    leading: Icon(Icons.home, color: Colors.white),
+                    title: Text('Home', style: TextStyle(
+                        color: Colors.white
+                    ),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: ListTile(
+                    leading: Icon(Icons.info, color: Colors.white),
+                    title: Text('About', style: TextStyle(
+                        color: Colors.white
+                    ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Divider(
-                color: Colors.grey[900],
+            const Padding(
+              padding: const EdgeInsets.only(left: 25, bottom: 25),
+              child: ListTile(
+                leading: Icon(Icons.logout, color: Colors.white),
+                title: Text('Logout', style: TextStyle(
+                    color: Colors.white
+                ),
+                ),
               ),
             )
           ],
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,12 +1,81 @@
+import 'package:e_commerce/models/shoe.dart';
 import 'package:flutter/material.dart';
+
+import '../components/shoe_tile.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('shop'),
+    return Column(
+      children: [
+        // search box
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+              color: Colors.grey[200], borderRadius: BorderRadius.circular(8)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                'Search',
+                style: TextStyle(color: Colors.grey),
+              ),
+              Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ),
+        // message
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: Text(
+            'everyone flies.. some fly longer than others',
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+        ),
+        // hot picks
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: const [
+              Text(
+                'Hot Picks 🔥',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
+              Text(
+                'See all',
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              Shoe shoe = Shoe(
+                  name: 'Air Jordan',
+                  price: '240',
+                  imagePath: 'lib/images/black_air.jpg',
+                  description: 'Cool shoe');
+              return ShoeTile(
+                shoe: shoe,
+              );
+            },
+          ),
+        )
+      ],
     );
   }
 }
